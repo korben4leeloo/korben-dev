1028f405dc2efd0bfe5fddadc496463df8e8775d
korben4leeloo <korben4leeloo@hotmail.com>
2016-06-20 16:50:13 +0200

    Save

---
+++ TARS/TARS.Core/Sources/File/TkBinaryFile.h
@@ -0,0 +1,21 @@
+//*****************************************************************************
+//
+//	Class:		TkBinaryFile
+//
+//	Created:	2013-08-26
+//
+//*****************************************************************************
+
+#ifndef __TARS_CORE_TK_BINARY_FILE_H__
+#define __TARS_CORE_TK_BINARY_FILE_H__
+
+class TkBinaryFile
+{
+public:
+			TkBinaryFile();
+			~TkBinaryFile();
+
+private:
+};
+
+#endif
---
+++ TARS/TARS.Core/Sources/File/TkTextFile.h
@@ -0,0 +1,23 @@
+//*****************************************************************************
+//
+//	Class:		TkEventManager
+//
+//	Created:	2013-08-26
+//
+//*****************************************************************************
+
+#ifndef __TARS_CORE_TK_EVENT_MANAGER_H__
+#define __TARS_CORE_TK_EVENT_MANAGER_H__
+
+class TkEventManager
+{
+public:
+			TkEventManager();
+			~TkEventManager();
+
+	void	update();
+
+private:
+};
+
+#endif
