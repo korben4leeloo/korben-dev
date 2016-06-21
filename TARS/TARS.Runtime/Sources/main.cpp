e912bf69ae3c47cd8ccaeb9bf1c3e03a13a7d8b3
korben4leeloo <korben4leeloo@hotmail.com>
2016-06-21 17:45:46 +0200

    Save

--- TARS/TARS.Runtime/Sources/main.cpp
+++ TARS/TARS.Runtime/Sources/main.cpp
@@ -10,6 +10,7 @@
 #include <Windows.h>
 
 #include TARS_CORE_H(Application/TkWin32App)
+#include TARS_CORE_H(File/TkBinaryFile)
 
 //-----------------------------------------------------------------------------
 // Name:		WinMain
@@ -20,8 +21,19 @@ int WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in_op
 {
 	TkWin32App* pWin32App = new TkWin32App( hInstance );
 
+	TkBinaryFile f;
+	TkString s( "prout" );
+
+	f.open( "D:/test.txt" );
+
+	f << 'h' << "\n";
+	f << 123;
+	f << s << "\n";
+
+	f.close();
+
 	pWin32App->createWindow();
-	pWin32App->runMessageLoop();	
+	pWin32App->runMessageLoop();
 
 	return 0;
 }
